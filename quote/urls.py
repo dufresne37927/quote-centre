4e65db48299cabfb9416cb7cc078e411b0a39838
dufresne37927 <steveywilson@gmail.com>
2022-10-06 20:55:57 +0100

    Implemented registration page.
    Modified appearance of register and login to match.
    
    Protected welcome page.

--- quote/urls.py
+++ quote/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.WelcomeView.as_view(), name="welcome"),
+    path("register/", views.register, name="register"),
 ]
--- quote/views.py
+++ quote/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class WelcomeView(TemplateView):
+class WelcomeView(LoginRequiredMixin, TemplateView):
     template_name = "quote/welcome.html"
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('welcome')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
